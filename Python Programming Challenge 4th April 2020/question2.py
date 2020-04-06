08d84814074d207d0bd998eb5e55c504953af9a5
rahul96rajan <rahulrajanqa@gmail.com>
2020-04-06 22:07:16 +0530

    Initial commit of Programming Challenge 04/04/2020

---
+++ Python Programming Challenge 4th April 2020/question2.py
@@ -0,0 +1,17 @@
+"""
+Problem  2.
+Jadoo, the space alien has challenged Koba to a friendly duel.
+He asks Koba to write a program to print out all numbers from 70 to 80.
+Knowing perfectly well how easy the problem is, the Jadoo adds his own twist
+to the challenge. He asks Koba to write the program without using a single
+number in his program and also restricts the character limit to 100.
+"""
+
+
+def get_70_to_80():
+    "returns a string a numbers between 70 to 80"
+    return ", ".join([str(x) for x in range(ord('F'), ord('P'))])
+
+
+if __name__ == "__main__":
+    print(get_70_to_80())
