458d804256acc1453ee7577a8a46dd002b70d13c
rahul96rajan <rahulrajanqa@gmail.com>
2020-08-06 09:05:03 +0530

    Added prolog directory and Q1

---
+++ Prolog/Q1_animal.pl
@@ -0,0 +1,7 @@
+/* Write a program to put facts indicating that a lion, a tiger and a cow are animals into the database and to record that two of them (lion and tiger) are carnivores. */
+
+animal(cow).
+animal(lion).
+animal(tiger).
+carnivore(tiger).
+carnivore(lion).
